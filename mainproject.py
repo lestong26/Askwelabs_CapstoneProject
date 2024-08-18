0f350a69a73443543e19e55008bfcd1a439aec5e
Carlos Lester Covarrubias II <clfinancialxxvi@gmail.com>
2024-08-19 07:10:43 +0800

    Update mainproject.py
    included 
    __import__('pysqlite3')
    import sys
    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')

--- mainproject.py
+++ mainproject.py
@@ -1,3 +1,6 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from streamlit_option_menu import option_menu
 from langchain.text_splitter import RecursiveCharacterTextSplitter
