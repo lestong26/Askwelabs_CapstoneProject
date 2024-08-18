b34d9f2e6ccf31855494e7eb14493b33a8df4e7d
Carlos Lester Covarrubias II <clfinancialxxvi@gmail.com>
2024-08-19 06:57:58 +0800

    Update mainproject.py
    langchain_chroma to from langchain.vectorstores import Chroma

--- mainproject.py
+++ mainproject.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_core.prompts import MessagesPlaceholder
